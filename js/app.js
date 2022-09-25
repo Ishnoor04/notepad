f14dd5635da254b7b0960180ae4b799cc740b794
newuserel <cleomitu@gmail.com>
2022-09-25 19:27:53 +0530

    Working ADD NOte FUnction

--- js/app.js
+++ js/app.js
@@ -1,17 +1,12 @@
-let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener("click", function(e) {
-  let addTxt = document.getElementById("addTxt");
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
-  notesObj.push(addTxt.value);
-  localStorage.setItem("notes", JSON.stringify(notesObj));
-  addTxt.value = "";
-  console.log(notesObj);
-  //showNotes();
-});
+function pushData(){
+    var text= document.getElementById("addTxt").value;
+ 
+     var array = [];
+ 
+     array.push(text); 
+ 
+     console.log(array.toString());
+ }
+
 
 
